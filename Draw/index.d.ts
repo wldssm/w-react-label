56b91eaba688502a55fed98f10b62a0f3b2606c6
wang389846 <wang389846@seeking.ai>
2024-06-06 17:54:22 +0800

    Updates

--- Draw/index.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import './index.css';
-declare const WDraw: React.ForwardRefExoticComponent<Omit<any, "ref"> & React.RefAttributes<unknown>>;
-export default WDraw;
--- index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as WColorBlock } from './ColorBlock';
-export { default as WDraw } from './Draw';
-export { default as WShapeList } from './ShapeList';
