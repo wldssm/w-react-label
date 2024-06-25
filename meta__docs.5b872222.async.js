28a2e864bc3a743d8b50237c2564f36827b85f43
wang389846 <wang389846@seeking.ai>
2024-06-25 17:08:28 +0800

    Updates

---
+++ meta__docs.5b872222.async.js
@@ -0,0 +1,7 @@
+"use strict";(self.webpackChunkw_react_label=self.webpackChunkw_react_label||[]).push([[904],{2687:function(n,a,e){e.r(a),e.d(a,{texts:function(){return t}});const t=[{value:"svg \u7ED8\u5236\u77E9\u5F62\u3001\u6B63\u65B9\u5F62\u3001\u692D\u5706\u5F62\u3001\u5706\u5F62\u3001\u591A\u8FB9\u5F62\u3001\u8DEF\u5F84\u3002\u57FA\u4E8E react16+\uFF0C\u4E0D\u517C\u5BB9 18+\u3002",paraId:0,tocIndex:0},{value:`yarn add w-react-label
+`,paraId:1,tocIndex:1},{value:`import { WDraw } from 'w-react-label';
+
+export default () => {
+  return <WDraw drawTool="rect" />;
+};
+`,paraId:2,tocIndex:2}]}}]);
